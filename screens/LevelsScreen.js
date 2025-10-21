6d0e89161c32d27ef5fe0ef0a31672eaab7a5b6f
misiatais <166124468+misiatais@users.noreply.github.com>
2025-10-21 17:14:35 -0300

    Remove comment regarding background bee image
    Removed comment about bee image in LevelsScreen.

--- screens/LevelsScreen.js
+++ screens/LevelsScreen.js
@@ -1,4 +1,4 @@
-// screens/LevelsScreen.js - COM IMAGEM DA ABELHA
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Mybutton from '../components/button';
@@ -13,7 +13,7 @@ export default function LevelsScreen() {
 
   return (
     <View style={styles.container}>
-      {/* Imagem da abelha de fundo */}
+   
       <Image
         source={require('../assets/abelha.png')}
         style={styles.backgroundImage}
